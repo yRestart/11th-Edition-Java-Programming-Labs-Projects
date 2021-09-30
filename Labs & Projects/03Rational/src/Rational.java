7c9e8b7110c119dd2068553727eed0d0275cf072
yRestart <fatlindbeka513@gmail.com>
2021-09-29 23:29:37 -0400

    added new methods, pow, subtract, reciprocal, and dividing to Rational class

--- Labs & Projects/03Rational/src/Rational.java
+++ Labs & Projects/03Rational/src/Rational.java
@@ -1,5 +1,5 @@
 public class Rational {
-	// data	
+	// data
 	private int num;
 	private int den;
 
@@ -7,13 +7,13 @@ public class Rational {
 	/**
 	 * Constructs a Rational from a numerator and denominator
 	 *
-	 * @param  n    the numerator
-	 * @param  d    the denominator
+	 * @param n the numerator
+	 * @param d the denominator
 	 */
-	public Rational(int n, int d){
-		if (d==0) {
+	public Rational(int n, int d) {
+		if (d == 0) {
 			System.out.println("Cannot divide by 0");
-			d=1;
+			d = 1;
 		}
 		num = n;
 		den = d;
@@ -23,7 +23,7 @@ public class Rational {
 	/**
 	 * Constructs a Rational from an integer
 	 *
-	 * @param  i    the integer. numerator=i, denominator = 1.
+	 * @param i the integer. numerator=i, denominator = 1.
 	 */
 	public Rational(int i) {
 		this(i, 1);
@@ -33,18 +33,18 @@ public class Rational {
 	/**
 	 * gets numerator
 	 *
-	 * @return      numerator
+	 * @return numerator
 	 */
-	public int getNumerator(){
+	public int getNumerator() {
 		return num;
 	}
 
 	/**
 	 * gets denominator
 	 *
-	 * @return      denominator
+	 * @return denominator
 	 */
-	public int getDenominator(){
+	public int getDenominator() {
 		return den;
 	}
 
@@ -52,7 +52,7 @@ public class Rational {
 	/**
 	 * Changes numerator, leaving denominator the same
 	 *
-	 * @param  n    the new numerator
+	 * @param n the new numerator
 	 */
 	public void setNumerator(int n) {
 		num = n;
@@ -62,10 +62,10 @@ public class Rational {
 	/**
 	 * Changes denominator, leaving numerator the same
 	 *
-	 * @param  d    the new denominator
+	 * @param d the new denominator
 	 */
 	public void setDenominator(int d) {
-		if (d==0)
+		if (d == 0)
 			System.out.println("Cannot divide by 0");
 		else {
 			den = d;
@@ -77,30 +77,29 @@ public class Rational {
 	/**
 	 * Formats a rational for Strings and printing
 	 *
-	 * @return      if denominator==0, numerator. otherwise, numerator/denominator.
+	 * @return if denominator==0, numerator. otherwise, numerator/denominator.
 	 */
-	public String toString(){
+	public String toString() {
 		// use getNumerator() not num. allows easy implementation changes.
-		int n = getNumerator(); 
+		int n = getNumerator();
 		int d = getDenominator();
-		if (d==1)
-			return n+"";
+		if (d == 1)
+			return n + "";
 		else
-			return n + "/" +d;
+			return n + "/" + d;
 	}
 
 	/**
-	 * Determines if two rational objects represent the same number.
-	 * Both numerator and denominator must be the same.
+	 * Determines if two rational objects represent the same number. Both numerator
+	 * and denominator must be the same.
 	 *
-	 * @param  r    the rational number to compare against
-	 * @return      true if same. false if not.
+	 * @param r the rational number to compare against
+	 * @return true if same. false if not.
 	 */
 	public boolean equals(Object o) {
-		if (o instanceof Rational){
+		if (o instanceof Rational) {
 			Rational r = (Rational) o;
-			return getNumerator()==r.getNumerator() && 
-					getDenominator()==r.getDenominator();
+			return getNumerator() == r.getNumerator() && getDenominator() == r.getDenominator();
 		} else
 			return false;
 	}
@@ -109,8 +108,8 @@ public class Rational {
 	/**
 	 * Multiplies one rational by another.
 	 *
-	 * @param  r    the rational number to multiply by
-	 * @return      the resulting rational
+	 * @param r the rational number to multiply by
+	 * @return the resulting rational
 	 */
 	public Rational multiply(Rational r) {
 		int n = getNumerator() * r.getNumerator();
@@ -118,34 +117,91 @@ public class Rational {
 		Rational product = new Rational(n, d);
 		return product;
 	}
+
 	/**
 	 * Adds one rational by another.
 	 *
-	 * @param  r    the rational number to add
-	 * @return      the resulting rational
+	 * @param r the rational number to add
+	 * @return the resulting rational
 	 */
 	public Rational add(Rational r) {
 		int n1 = getNumerator();
 		int n2 = r.getNumerator();
 		int d1 = getDenominator();
 		int d2 = r.getDenominator();
-		Rational product = new Rational(n1*d2 + n2*d1, d1*d2);
+		Rational product = new Rational(n1 * d2 + n2 * d1, d1 * d2);
 		return product;
 	}
+
+	// Subtract
+	public Rational subtract(Rational r) {
+		int fOneTemp = r.getDenominator() * getNumerator();
+		int fTwoTemp = getDenominator() * r.getNumerator();
+		
+		int n = fOneTemp - fTwoTemp;
+		int d = getDenominator() * r.getDenominator();
+		
+		Rational subtract = new Rational(n, d);
+		return subtract;
+	}
+
+	// Divide
+	public Rational divide(Rational r) {
+		int n = getNumerator() * r.getDenominator();
+		int d = getDenominator() * r.getNumerator();
+		Rational divide = new Rational(n, d);
+		return divide;
+	}
+
+	// Reciprocal
+	public Rational getReciprocal() {
+		int n = getDenominator();
+		int d = getNumerator();
+		Rational reciprocal = new Rational(n, d);
+		return reciprocal;
+	}
+
+	// pow
+	public double power(Rational r) {
+
+		double powOneNum = getNumerator();
+		double powOneDenom = getDenominator();
+		double powOneCalc = powOneNum / powOneDenom;
+
+		double powTwoNum = r.getNumerator();
+		double powTwoDenom = r.getDenominator();
+		double powTwoCalc = powTwoNum / powTwoDenom;
+
+		double power = Math.pow(powOneCalc, powTwoCalc);
+		return power;
+	}
 	
-	// utility methods, not for public use	
+	// Checking if equal
+	public String isEqual(Rational r) {
+		if(getNumerator() == r.getNumerator() && getDenominator() == r.getDenominator()) {
+			String x = "The rational numbers are equal to each other.";
+			return x;
+		}else {
+			String z = "The rational numbers are not equal to each other.";
+			return z;
+		}
+		
+	}
+
+	// utility methods, not for public use
 	private void simplify() {
 		int n = getNumerator();
 		int d = getDenominator();
 		int gcd = GCD(n, d);
-		num = n/gcd;
-		den = d/gcd;
+		num = n / gcd;
+		den = d / gcd;
 	}
 
 	private int GCD(int a, int b) {
-		if (b==0) 
+		if (b == 0)
 			return a;
 		else
-			return GCD(b,a%b);
+			return GCD(b, a % b);
 	}
+
 }
--- Labs & Projects/03Rational/src/RationalApp.java
+++ Labs & Projects/03Rational/src/RationalApp.java
@@ -5,28 +5,38 @@ public class RationalApp {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		int denominator, numerator;
+		int denomOne, numOne, numTwo, denomTwo;
 		System.out.printf("Enter a numerator: ");
-		numerator = input.nextInt();
+		numOne = input.nextInt();
 		System.out.printf("Enter a denominator: ");
-		denominator = input.nextInt();
+		denomOne = input.nextInt();
+		Rational rationalOne = new Rational(numOne, denomOne);
+		System.out.printf("%nNumerator: %d", rationalOne.getNumerator());
+		System.out.printf("%nDenominator: %d", rationalOne.getDenominator());
+		
 
-		Rational rational = new Rational(numerator, denominator);
+		
+		System.out.printf("%n%nEnter a numerator: ");
+		numTwo = input.nextInt();
+		System.out.printf("Enter a denominator: ");
+		denomTwo = input.nextInt();
+		Rational rationalTwo = new Rational(numTwo, denomTwo);
+		System.out.printf("%nNumerator: %d", rationalTwo.getNumerator());
+		System.out.printf("%nDenominator: %d", rationalTwo.getDenominator());
+	
+		
+		
+		System.out.printf("%n%n%s",rationalOne.isEqual(rationalTwo));
+		System.out.printf("%nThe sum of the rational numbers is: %s", rationalOne.add(rationalTwo));
+		System.out.printf("%nThe difference of the rational numbers is: %s", rationalOne.subtract(rationalTwo));
+		System.out.printf("%nThe product of the rational numbers is: %s", rationalOne.multiply(rationalTwo));
+		System.out.printf("%nThe quotient of the rational numbers is: %s", rationalOne.divide(rationalTwo));
+		System.out.printf("%nThe reciprocal of the first rational is: %s", rationalOne.getReciprocal());
+		System.out.printf("%nThe reciprocal of the second rational is: %s", rationalTwo.getReciprocal());
+		System.out.printf("%nThe first raised to the power of the second is: %.2f", rationalOne.power(rationalTwo));
+		
 
-		System.out.printf("Numerator: %d", rational.getNumerator());
-		System.out.printf("%nDenominator: %d", rational.getDenominator());
-
-		/*
-		 * The rational numbers are not equal to each other. 
-		 * The sum of the rational numbers is: 13/10 
-		 * The product of the rational numbers is: 2/5 
-		 * The first raised to the power of the second is: 0.57 
-		 * The difference of the rational numbers is: -3/10 
-		 * The reciprocal of the first rational is: 2 
-		 * The reciprocal of the second rational is: 5/4 
-		 * The quotient of the rational numbers is: 5/8
-		 */
-
+		input.close();
 	}
 
 }
